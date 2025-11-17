565a7ba33484ced6dd0ba299b68f2b28a004e44f
sinwooKim <rlatlsdn98@naver.com>
2025-11-17 17:06:39 +0900

    ADD ERD modify README.md

--- DML/sign_in.sql
+++ DML/sign_in.sql
@@ -2,6 +2,6 @@
 회원가입
  */
 
--- DEFAULT, NULL 제약조건은 -> 선택사항(입력 불필요)
+-- DEFAULT, NULL 제약조건은 -> 선택사항(입력 불필요), 필수적인 칼럼만 표시해서 INSERT
 INSERT INTO USER (user_id, city_id, name, gender, birth_date, email, phone) VALUES
 (null, null,'김신우','M','1998-01-12','rlatlsdn98@gmail.com','010-7611-7026');
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Trip.mac 유스케이스 다이어그램
-![251117_2TEAM_usecase.drawio.png](img/251117_2TEAM_usecase.drawio.png)
+![251117_2TEAM_usecase.drawio.png](img/2team_usecase_diagram.png)
+
+# ERD
